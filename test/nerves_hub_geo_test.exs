38321f4ef61c9e384511bceea2e871d8fb829da1
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-07-16 11:35:42 +1200

    mimic is working!

--- test/nerves_hub_geo_test.exs
+++ test/nerves_hub_geo_test.exs
@@ -58,24 +58,27 @@ defmodule NervesHubLinkGeoTest do
     test "server requests geo location" do
       Application.put_env(:nerves_hub_link_geo, :resolver, TestResolver)
 
-      Socket
-      |> expect(:send_message, fn "device", "location:update", %{} -> dbg() end)
+      expect(Socket, :send_message, fn "device", "location:update", _location ->
+        :ok
+      end)
 
-      PubSub.subscribe("device")
+      allow(Socket, self(), Process.whereis(NervesHubLinkGeo))
 
       # Emulate nerves_hub_link passing us a server event
       PubSub.publish_channel_event("device", "location:request", %{})
-
-      assert_receive %PubSub.Message{
-        type: :msg,
-        topic: "device",
-        event: "location:request",
-        params: %{}
-      }
     end
 
-    # test "geo location sent after the device has joined" do
-    #   # Need to figure this out
-    # end
+    test "sends location after the device has joined" do
+      Application.put_env(:nerves_hub_link_geo, :resolver, TestResolver)
+
+      expect(Socket, :send_message, fn "device", "location:update", _location ->
+        :ok
+      end)
+
+      allow(Socket, self(), Process.whereis(NervesHubLinkGeo))
+
+      # Emulate nerves_hub_link announce that the device has joined the channel
+      PubSub.publish_topic_join("device", %{})
+    end
   end
 end
