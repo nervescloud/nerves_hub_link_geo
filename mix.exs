1b54d58c56f7b1587a73714091a1e6df9af7a63a
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-07-16 22:24:38 +1200

    use the mix functions

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule NervesHubLinkGeo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
